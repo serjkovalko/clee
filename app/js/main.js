d073b4c02ba861338e8ce21caf333d69476c9bc1
serjkovalko <serjkovalko@gmail.com>
2021-03-27 21:44:28 +0200

    added product lict 1

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,25 @@
 $(function () {
 
+  $('.filter-price__input').ionRangeSlider({
+    type: "double",
+    prefix: "$",
+    onStart: function (data){
+      $('.filter-price__from').text(data.from);
+      $('.filter-price__to').text(data.to);
+    },
+    onChange: function (data) {
+      $('.filter-price__from').text(data.from);
+      $('.filter-price__to').text(data.to);
+    },
+  });
+
+  $(".star").rateYo({
+    starWidth: "12px",
+    normalFill: "#d6d6d6",
+    ratedFill: "#ffcc00",
+    readOnly: true
+  });
+
   $('.top-slider__inner').slick({
      dots: true,
      arrows: false,
