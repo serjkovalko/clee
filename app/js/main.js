2158539103d839c5b4da3d30abcf60ab92aff4bc
serjkovalko <serjkovalko@gmail.com>
2021-03-28 20:07:58 +0300

    added product list 2

--- app/js/main.js
+++ app/js/main.js
@@ -20,13 +20,7 @@ $(function () {
     readOnly: true
   });
 
-  $('.top-slider__inner').slick({
-     dots: true,
-     arrows: false,
-     fade: true,
-     autoplay: true,
-     autoplaySpeed: 3000
-  });
+
   var mixer = mixitup('.categories-products');
   var mixer = mixitup('.design__items');
 });
