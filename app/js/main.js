1a37a6e54419172282a99c1d7a40b96c34587df9
serjkovalko <serjkovalko@gmail.com>
2021-03-29 22:09:08 +0300

    added Product Detalis page

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,23 @@
 $(function () {
 
+  $('.product-one__item-num').styler();
+
+  $('.product-slide__thumb').slick({
+    asNavFor: '.product-slide__big',
+    focusOnSelect: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    vertical: true,
+    draggable: false
+  });
+
+  $('.product-slide__big').slick({
+    asNavFor: '.product-slide__thumb',
+    draggable: false,
+    arrows: false,
+    fade: true
+  });
+
   $('.filter-price__input').ionRangeSlider({
     type: "double",
     prefix: "$",
@@ -11,6 +29,14 @@ $(function () {
       $('.filter-price__from').text(data.from);
       $('.filter-price__to').text(data.to);
     },
+  });
+
+  $('.top-slider__inner').slick({
+    dots: true,
+    arrows: false,
+    fade: true,
+    autoplay: true,
+    autoplaySpead: 2000
   });
 
   $(".star").rateYo({
