4aa023d43b6d90109f5acc655433a5361f0f7624
serjkovalko <serjkovalko@gmail.com>
2021-03-30 18:01:00 +0300

    added section related

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,19 @@
 $(function () {
 
+  $('.related-top__arrow').on('click', function(){
+    $('.related-top__arrow').removeClass('related-top__arrow--active');
+    $(this).addClass('related-top__arrow--active');
+  });
+
+  $('.product-tabs__top-item').on('click', function(e){
+   e.preventDefault();
+    $('.product-tabs__top-item').removeClass('product-tabs__top-item--active');
+    $(this).addClass('product-tabs__top-item--active');
+
+    $('.product-tabs__content-item').removeClass('product-tabs__content-item--active');
+    $($(this).attr('href')).addClass('product-tabs__content-item--active');
+  });
+
   $('.product-one__item-num').styler();
 
   $('.product-slide__thumb').slick({
